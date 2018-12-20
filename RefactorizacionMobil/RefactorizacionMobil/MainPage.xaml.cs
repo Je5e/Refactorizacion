852356ea99243bc032c6e983a8efc45a46bab00b
unknown <jalvarado@univo.edu.sv>
2018-12-20 11:08:24 -0600

    Initial commit

---
+++ RefactorizacionMobil/RefactorizacionMobil/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RefactorizacionMobil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonAddClicked(object sender, EventArgs args)
+        {
+            Operaciones OP = new Operaciones();
+            var Value1 =int.Parse( EntryValue1.Text);
+            var Value2 = int.Parse(EntryValue2.Text);
+            try
+            {
+                LabelResult.Text = OP.Add(Value1, Value2).ToString();
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                DisplayAlert("Divided By Zero", ex.Message, "Ok");
+            }
+            catch(FileNotFoundException)
+            {
+
+            }
+           
+
+        }
+    }
+}
---
+++ RefactorizacionMobil/RefactorizacionMobil/Operaciones.cs
@@ -0,0 +1,18 @@
+﻿namespace RefactorizacionMobil
+{
+    // Exponer funcionalidad de Sumar valor1 y valor2
+    public class Operaciones
+    {
+        public int Add(int V1, int V2)
+        {
+            int Value1 = V1;
+            int Value2 = V2;
+            if (Value2 == 0)
+            {
+                throw new System.DivideByZeroException();
+            }
+            int Result = Value1 / Value2;
+            return Result;
+        }
+    }
+}
